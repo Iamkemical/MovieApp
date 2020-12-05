8a8a35d050072a7b0fb94a83d40f82fc4fad1198
Gabriel Isaac <iamkemical1@gmail.com>
2020-12-05 11:01:29 +0100

    Added Nuget packages to the pruject for database

--- MovieApp/Models/GenreModel.cs
+++ MovieApp/Models/GenreModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,10 @@ namespace MovieApp.API.Models
 {
     public class GenreModel
     {
-
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
     }
 }
