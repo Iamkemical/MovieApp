02e6484f2d2fd11171704eb9993a44baa2589021
Gabriel Isaac <iamkemical1@gmail.com>
2020-12-05 10:52:21 +0100

    Added Web API Project

--- MovieApp/Program.cs
+++ MovieApp/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace MovieApp
+namespace MovieApp.API
 {
     public class Program
     {
--- MovieApp/Startup.cs
+++ MovieApp/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace MovieApp
+namespace MovieApp.API
 {
     public class Startup
     {
