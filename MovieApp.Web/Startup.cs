a2f5cb1c2ae126bf10bc6a5b789ec84d75b59cde
Gabriel Isaac <iamkemical1@gmail.com>
2020-12-06 12:15:44 +0100

    Registered HttpClient in the Startup.cs for DI

--- MovieApp.Web/Startup.cs
+++ MovieApp.Web/Startup.cs
@@ -24,6 +24,7 @@ namespace MovieApp.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
