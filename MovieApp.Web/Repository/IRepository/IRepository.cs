e093fcd7d04b5b981832d3305f896104961e3b29
Gabriel Isaac <iamkemical1@gmail.com>
2020-12-06 12:28:07 +0100

    Created IRepository and Implemented the Repository

---
+++ MovieApp.Web/Repository/IRepository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Web.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetAsync(string url, int id, string token);
+        Task<IEnumerable<T>> GetAllAsync(string url, string token);
+        Task<bool> CreateAsync(string url, T objToCreate, string token);
+        Task<bool> UpdateAsync(string url, T objToUpdate, string token);
+        Task<bool> DeleteAsync(string url, int id, string token);
+    }
+}
---
+++ MovieApp.Web/Repository/Repository.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using MovieApp.Web.Repository.IRepository;
+using Newtonsoft.Json;
+
+namespace MovieApp.Web.Repository
+{
+    public class Repository<T> : IRepository<T> where T: class
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        public Repository(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+        public async Task<bool> CreateAsync(string url, T objToCreate, string token="")
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            if (objToCreate != null)
+            {
+                request.Content = new StringContent(
+                    JsonConvert.SerializeObject(objToCreate), Encoding.UTF8, "application/json");
+            }
+            else
+            {
+                return false;
+            }
+
+            var client = _clientFactory.CreateClient();
+            if (token.Length != 0)
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            HttpResponseMessage response = await client.SendAsync(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.Created)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> DeleteAsync(string url, int id, string token ="")
+        {
+            var request = new HttpRequestMessage(HttpMethod.Delete, url + id);
+
+            var client = _clientFactory.CreateClient();
+
+            if (token != null && token.Length != 0)
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            HttpResponseMessage response = await client.SendAsync(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(string url, string token="")
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+            var client = _clientFactory.CreateClient();
+
+            if (token != null && token.Length != 0)
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            HttpResponseMessage response = await client.SendAsync(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<IEnumerable<T>>(jsonString);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public async Task<T> GetAsync(string url, int id, string token="")
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, url + id);
+
+            var client = _clientFactory.CreateClient();
+
+            if (token != null && token.Length != 0)
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            HttpResponseMessage response = await client.SendAsync(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<T>(jsonString);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public async Task<bool> UpdateAsync(string url, T objToUpdate, string token="")
+        {
+            var request = new HttpRequestMessage(HttpMethod.Patch, url);
+            if (objToUpdate != null)
+            {
+                request.Content = new StringContent(
+                    JsonConvert.SerializeObject(objToUpdate), Encoding.UTF8, "application/json");
+            }
+            else
+            {
+                return false;
+            }
+
+            var client = _clientFactory.CreateClient();
+
+            if (token != null && token.Length != 0)
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            HttpResponseMessage response = await client.SendAsync(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
