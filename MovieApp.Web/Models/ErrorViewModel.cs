3bc934695f3d86ffb05fedaed09a9829f10c5e01
Gabriel Isaac <iamkemical1@gmail.com>
2020-12-06 11:58:00 +0100

    Added MVC Project for Consuming API

---
+++ MovieApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MovieApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
