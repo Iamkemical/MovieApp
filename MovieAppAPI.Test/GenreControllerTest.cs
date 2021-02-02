6c7828e6932a4dea96ab001d3651b3a45dbfac4a
Gabriel Isaac <iamkemical1@gmail.com>
2021-02-02 12:49:38 +0100

    Added Unit Test Project for API

---
+++ MovieAppAPI.Test/GenreControllerTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MovieAppAPI.Test
+{
+    public class GenreControllerTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
