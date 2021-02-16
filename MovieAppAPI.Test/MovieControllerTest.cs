40fb28606d76fe0c93d8d99eb3036368a4bc2e44
Gabriel Isaac <iamkemical1@gmail.com>
2021-02-16 13:54:03 +0100

    Added Unit Test for Movie API Controller Patch Request

--- MovieAppAPI.Test/MovieControllerTest.cs
+++ MovieAppAPI.Test/MovieControllerTest.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using MovieApp.API.Controllers;
@@ -142,6 +143,45 @@ namespace MovieAppAPI.Test
             movieRepositoryMock.Setup(repo => repo.UpdateMovie(It.IsAny<MovieModel>())).Returns(true);
             // Act
             var subGenreResult = movieApiController.UpdateMovie(movieModel.Id, movieDto);
+            var noContentResult = subGenreResult as NoContentResult;
+
+            // Assert
+            Assert.True(noContentResult.StatusCode is StatusCodes.Status204NoContent);
+        }
+
+        [Fact]
+        public void PartialUpdateMovie_Returns_NoContentResult()
+        {
+            // Arrange
+            var movieRepositoryMock = new Mock<IMovieRepository>();
+            var subGenreIMapperMock = new MapperConfiguration(config =>
+            {
+                config.AddProfile(new MovieMapper());
+            });
+            var movieMapper = subGenreIMapperMock.CreateMapper();
+            MoviesController movieApiController = new MoviesController(movieRepositoryMock.Object, mapper: movieMapper);
+            var patchDoc = new JsonPatchDocument<MoviesUpdateDTO>();
+            var movieModel = new MovieModel()
+            {
+                Name = "Adult Content",
+                DateCreated = DateTime.Parse("15 May 2015"),
+                Id = Guid.NewGuid(),
+                GenreId = Guid.NewGuid(),
+                Genres = new GenreModel(),
+            };
+            var movieDto = new MoviesUpdateDTO()
+            {
+                Id = movieModel.Id,
+                Name = movieModel.Name,
+                DateCreated = movieModel.DateCreated,
+                GenreId = movieModel.GenreId,
+                SubGenreId = Guid.NewGuid()
+            };
+            movieRepositoryMock.Setup(repo => repo.GetMovie(It.IsAny<Guid>())).Returns(movieModel);
+            patchDoc.ApplyTo(movieDto, movieApiController.ModelState);
+            movieRepositoryMock.Setup(repo => repo.UpdateMovie(It.IsAny<MovieModel>())).Returns(true);
+            // Act
+            var subGenreResult = movieApiController.PartialUpdateMovie(movieModel.Id, patchDoc);
             var noContentResult = subGenreResult as NoContentResult;
 
             // Assert
