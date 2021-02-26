59a547841826741c2fa60652ad3657be285bf22a
Gabriel Isaac <iamkemical1@gmail.com>
2021-02-26 14:18:27 +0100

    Configured Serilog into Program.cs

--- MovieApp/Program.cs
+++ MovieApp/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace MovieApp.API
 {
@@ -14,10 +15,23 @@ namespace MovieApp.API
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                .CreateLogger();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
